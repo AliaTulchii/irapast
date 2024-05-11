d2b91a939d57a2b307d05ab1c3102cf2b0fc74cb
Halina Tulchii <halendeihalyna@gmail.com>
2024-05-11 11:53:47 +0300

    checked errors added basename

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import "./styles/main.css";
-// import Header from "./components/header/Header";
-// import Footer from "./components/footer/Footer";
-// import Home from "./pages/Home";
 import { BrowserRouter as Router } from "react-router-dom";
-// import { motion } from 'framer-motion';
 import AnimRoutes from "./components/animroutes/AnimRoutes";
 
 
 function App() {
   return (
     <>      
-      <Router>
-        
-        <AnimRoutes />
-        
+      <Router basename="/irapast">        
+        <AnimRoutes />        
       </Router>
       
     </>
