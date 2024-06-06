8faf11515aee6fb71a0c8b005e375127fcbbf16e
Halina Tulchii <halendeihalyna@gmail.com>
2024-06-06 19:25:55 +0300

    resize photos

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import "./styles/main.css";
 import { React} from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AnimRoutes from "./components/animroutes/AnimRoutes";
-// import Loader from "./components/loader/Loader";
+
 
 
 function App() {
   return (
-    // <Suspense fallback={<Loader/>}>      
-      <Router basename="/irapast">        
+      <Router>        
         <AnimRoutes />        
       </Router>
-      
-    // </Suspense>
   );
 }
 
